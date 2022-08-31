222969ead7a36d703cebbf9a20f50ca4b410a2c7
semihar <semiharalem@gmail.com>
2022-08-30 20:15:40 -0400

    Utils are done but need to be initialized

--- src/test/java/pages/DashboardPage.java
+++ src/test/java/pages/DashboardPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.CommonMethods;
+
 import java.util.List;
 
 public class DashboardPage extends CommonMethods {
